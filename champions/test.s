2ffffeef66c15469677b105dd1184ee07b0a6a56
Lakhdar Lawrence <lbenamer@student.42.fr>
2017-07-13 15:34:42 +0200

    clear git

--- champions/test.s
+++
@@ -1,9 +0,0 @@
-.name "charles"
-.comment "il en a une grosse c bastien ki le dit"
-
-l2: sti r1,%:live,%1
-	and r1,%0,r1
-
-live: live %42
-	  zjmp %:live
-	  lld  42,r5
--- player/zork_2.s
+++
@@ -1,8 +0,0 @@
-.name "zork number 2"
-.comment "I'M ALIIIIVE"
-
-l2:		st r1, r6
-		and r1, %0, r1
-
-live:	live %1
-		zjmp %:live
